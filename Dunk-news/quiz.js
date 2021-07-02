625db204d0e55f354164601f33991c4762b43acc
matheuseps <matheusestevo2802@gmail.com>
2021-07-02 15:44:05 -0300

    js
    criei uma pagina em js

---
+++ Dunk-news/quiz.js
@@ -0,0 +1,9 @@
+function responder() {
+    const res = document.querySelector('.res')
+
+    const inputs = document.querySelectorAll('input')
+
+    if(inputs.value = 'false'){
+        res.innerHTML = (`<h1><strong> Você errou</strong></h1>`)
+    }
+}
