6ccb6a308ad25ba56d8aede4dc1848a674c1127d
matheuseps <matheusestevo2802@gmail.com>
2021-07-08 16:09:30 -0300

    js
    mudei o site

--- Dunk-news/quiz.js
+++ Dunk-news/quiz.js
@@ -1,9 +1,29 @@
-function responder() {
-    const res = document.querySelector('.res')
+function responder(){
+    const res = document.querySelector('#res')
+    const select = document.querySelector('#alternativas')
 
-    const inputs = document.querySelectorAll('input')
+    const respostasCorretas = [
+        '17',
+        'Abdul-Jabbar',
+        'Bill Russell'
+    ]
 
-    if(inputs.value = 'false'){
-        res.innerHTML = (`<h1><strong> Você errou</strong></h1>`)
+    if(select.value == respostasCorretas[0] || select.value == respostasCorretas[1] || select.value == respostasCorretas[2]){
+        res.innerHTML = (`<h1>Você acertou</h1> <strong><p>Parabéns</p></strong>  <p>Vá para próxima pergunta</p>`)
+    } else{
+        res.innerHTML = (`<h1>Você errou</h1> <br> <p>Tente novamente ou vá para próxima pergunta</p>`)
+    }
+}
+
+function finalizar(){
+    const res = document.querySelector('#res')
+    const select = document.querySelector('#alternativas')
+
+    const correta = 'Warriors 15/16'
+
+    if(select.value == correta){
+        res.innerHTML = (`<h1>Você acertou</h1> <strong><p>Parabéns</p></strong>  <p>Se você não foi bem no quiz, tente novamente clicando no botão abaixo.</p>`)
+    } else{
+        res.innerHTML = (`<h1>Você errou</h1> <br> <p>Tente novamente</p>`)
     }
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Dunk-News2
-Um site sobre basquete com noticias e etc.
